eec7109a48872f5b85b514e43fd6597eaf39c13f
Yunhe Gao <yg397@achilleas.cs.rutgers.edu>
2022-03-02 11:26:04 -0500

    update dataset

--- training/dataset/utils.py
+++ training/dataset/utils.py
@@ -8,11 +8,6 @@ def get_dataset(args, mode, **kwargs):
             from .dim2.dataset_acdc import CMRDataset
 
             return CMRDataset(args, mode=mode, k_fold=args.k_fold, k=kwargs['fold_idx'], seed=args.seed)
-        elif args.dataset == 'synapse':
-            from .dim2.dataset_synapse import SynapseDataset
-
-            return SynapseDataset(args, mode=mode, k_fold=args.k_fold, k=kwargs['fold_idx'], seed=args.seed)
-
 
     else:
         if args.dataset == 'acdc':
@@ -20,9 +15,4 @@ def get_dataset(args, mode, **kwargs):
 
             return CMRDataset(args, mode=mode)
 
-        elif args.dataset == 'synapse':
-            from .dim3.dataset_synapse import SynapseDataset
-            
-            return SynapseDataset(args, mode=mode, k_fold=args.k_fold, k=kwargs['fold_idx'], seed=args.seed)
-        
 
