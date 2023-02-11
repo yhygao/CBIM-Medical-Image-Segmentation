27f9f20cd3acb7d8b0cd78604f9e805253923700
Yunhe Gao <yunhe.gao@rutgers.edu>
2023-02-10 23:51:50 -0500

    fix axis order in the random affine augmentation

--- training/augmentation.py
+++ training/augmentation.py
@@ -177,8 +177,13 @@ def random_scale_rotate_translate_2d(tensor_img, tensor_lab, scale, rotate, tran
 
     return tensor_img, tensor_lab
 
-
 def random_scale_rotate_translate_3d(tensor_img, tensor_lab, scale, rotate, translate, noshear=True):
+    '''
+    The axis order of SimpleITK is x,y,z
+    The axis order of numpy/tensor is z,y,x
+    The arguments of all transformation should use the numpy/tensor order: [z,y,x]
+
+    '''
     
     if isinstance(scale, float) or isinstance(scale, int):
         scale = [scale] * 3
@@ -187,43 +192,46 @@ def random_scale_rotate_translate_3d(tensor_img, tensor_lab, scale, rotate, tran
     if isinstance(rotate, float) or isinstance(rotate, int):
         rotate = [rotate] * 3
 
-    scale_z = 1 - scale[0] + np.random.random() * 2*scale[0]
-    scale_x = 1 - scale[1] + np.random.random() * 2*scale[1]
-    scale_y = 1 - scale[2] + np.random.random() * 2*scale[2]
-    shear_xz = 0 if noshear else np.random.random() * 2*scale[0] - scale[0]
-    shear_yz = 0 if noshear else np.random.random() * 2*scale[0] - scale[0]
-    shear_zx = 0 if noshear else np.random.random() * 2*scale[1] - scale[1]
-    shear_yx = 0 if noshear else np.random.random() * 2*scale[1] - scale[1]
-    shear_zy = 0 if noshear else np.random.random() * 2*scale[2] - scale[2]
-    shear_xy = 0 if noshear else np.random.random() * 2*scale[2] - scale[2]
-    translate_z = np.random.random() * 2*translate[0] - translate[0]
-    translate_x = np.random.random() * 2*translate[1] - translate[1]
-    translate_y = np.random.random() * 2*translate[2] - translate[2]
+    scale_x = 1 - scale[0] + np.random.random() * 2*scale[0]
+    scale_y = 1 - scale[1] + np.random.random() * 2*scale[1]
+    scale_z = 1 - scale[2] + np.random.random() * 2*scale[2]
+    shear_yx = 0 if noshear else np.random.random() * 2*scale[0] - scale[0] # contribution of y index to x axis
+    shear_zx = 0 if noshear else np.random.random() * 2*scale[0] - scale[0] # contribution of z index to x axis
+    shear_xy = 0 if noshear else np.random.random() * 2*scale[1] - scale[1]
+    shear_zy = 0 if noshear else np.random.random() * 2*scale[1] - scale[1]
+    shear_xz = 0 if noshear else np.random.random() * 2*scale[2] - scale[2]
+    shear_yz = 0 if noshear else np.random.random() * 2*scale[2] - scale[2]
+    translate_x = np.random.random() * 2*translate[0] - translate[0]
+    translate_y = np.random.random() * 2*translate[1] - translate[1]
+    translate_z = np.random.random() * 2*translate[2] - translate[2]
 
 
-    theta_scale = torch.tensor([[scale_y, shear_xy, shear_zy, translate_y],
-                                [shear_yx, scale_x, shear_zx, translate_x],
-                                [shear_yz, shear_xz, scale_z, translate_z], 
+    theta_scale = torch.tensor([[scale_x, shear_yx, shear_zx, translate_x],
+                                [shear_xy, scale_y, shear_zy, translate_y],
+                                [shear_xz, shear_yz, scale_z, translate_z], 
                                 [0, 0, 0, 1]]).float()
-    angle_xy = (float(np.random.randint(-rotate[0], max(rotate[0], 1))) / 180.) * math.pi
-    angle_xz = (float(np.random.randint(-rotate[1], max(rotate[1], 1))) / 180.) * math.pi
-    angle_yz = (float(np.random.randint(-rotate[2], max(rotate[2], 1))) / 180.) * math.pi
+    angle_x = (float(np.random.randint(-rotate[0], max(rotate[0], 1))) / 180.) * math.pi 
+    # rotate along z axis (z index fix, rotae in xy plane)
+    angle_y = (float(np.random.randint(-rotate[1], max(rotate[1], 1))) / 180.) * math.pi
+    # rotate along y axis (y index fix, rotate in xz plane)
+    angle_z = (float(np.random.randint(-rotate[2], max(rotate[2], 1))) / 180.) * math.pi
+    # rotate along x axis (x index fix, rotate in yz plane)
     
-    theta_rotate_xz = torch.tensor([[1, 0, 0, 0],
-                                    [0, math.cos(angle_xz), -math.sin(angle_xz), 0],
-                                    [0, math.sin(angle_xz), math.cos(angle_xz), 0],
+    theta_rotate_x = torch.tensor([[1, 0, 0, 0],
+                                    [0, math.cos(angle_x), -math.sin(angle_x), 0],
+                                    [0, math.sin(angle_x), math.cos(angle_x), 0],
                                     [0, 0, 0, 1]]).float()
-    theta_rotate_xy = torch.tensor([[math.cos(angle_xy), -math.sin(angle_xy), 0, 0],
-                                    [math.sin(angle_xy), math.cos(angle_xy), 0, 0],
+    theta_rotate_y = torch.tensor([[math.cos(angle_y), 0, -math.sin(angle_y), 0],
+                                    [0, 1, 0, 0],
+                                    [math.sin(angle_y), 0, math.cos(angle_y), 0],
+                                    [0, 0, 0, 1]]).float()
+    theta_rotate_z = torch.tensor([[math.cos(angle_z), -math.sin(angle_z), 0, 0],
+                                    [math.sin(angle_z), math.cos(angle_z), 0, 0],
                                     [0, 0, 1, 0],
                                     [0, 0, 0, 1]]).float()
-    theta_rotate_yz = torch.tensor([[math.cos(angle_yz), 0, -math.sin(angle_yz), 0],
-                                    [0, 1, 0, 0],
-                                    [math.sin(angle_yz), 0, math.cos(angle_yz), 0],
-                                    [0, 0, 0, 1]]).float()
 
-    theta = torch.mm(theta_rotate_xy, theta_rotate_xz)
-    theta = torch.mm(theta, theta_rotate_yz)
+    theta = torch.mm(theta_rotate_x, theta_rotate_y)
+    theta = torch.mm(theta, theta_rotate_z)
     theta = torch.mm(theta, theta_scale)[0:3, :].unsqueeze(0)
     
     grid = F.affine_grid(theta, tensor_img.size(), align_corners=True)
@@ -232,6 +240,9 @@ def random_scale_rotate_translate_3d(tensor_img, tensor_lab, scale, rotate, tran
 
     return tensor_img, tensor_lab
     
+
+
+  
 
 def crop_2d(tensor_img, tensor_lab, crop_size, mode):
     assert mode in ['random', 'center'], "Invalid Mode, should be \'random\' or \'center\'"
